777fcd286a38579070b4bb040c991fd8b0efc613
Tebogochonco <115028731+Tebogochonco@users.noreply.github.com>
2023-01-27 23:27:31 +0200

    Calculator attempt
    first Attempt at the calculator

---
+++ js folder/ifCalculator.js
@@ -0,0 +1,18 @@
+//Marks
+//if the number on the scale is >6: Pass
+//Otherwise: Fail
+
+var myResults = console.log('>6 = Pass')
+let marks = 7
+
+
+    document.getElementById('myResults').innerHTML= 
+function myResults () {
+    if (marks >6) {
+        console.log('Pass!');
+    }
+    else (marks <6) 
+    console.log('Fail');
+
+}
+
